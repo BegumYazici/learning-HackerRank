c0dc0dc54b6e0ab094b5c90dcdb7416c56eef580
Begum.Yazici <yazicibegum@gmail.com>
2017-11-23 23:09:36 +0200

    Commited.

---
+++ hackerRank/src/HackerRankExamples/JavaTutorial/JavaWordCount.java
@@ -0,0 +1,18 @@
+package HackerRankExamples.JavaTutorial;
+
+public class JavaWordCount {
+
+    public static void main(String[] args) {
+
+        String metin = "Kullanicidan A'dan Z'ye kadar bir harf girmesini isteyin ve girilen harflere kadar olan harflerden olusan bir piramit seklinde cikti olusturun." +
+                " Piramitteki üst harf A olmalı ve her seviyede alfabedeki bir sonraki harf, ustteki seviyede tanitilan harfin iki kopyasına kadar dusmelidir.";
+
+        String[] dizim2 = metin.split(" ");
+
+        for (String j : dizim2)
+            System.out.println(j);
+
+        System.out.println("Metindeki kelime sayim: " + dizim2.length);
+    }
+}
+
